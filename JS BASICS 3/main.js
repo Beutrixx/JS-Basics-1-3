42fedfe72578c44d4c1c6ba23341933245801c8a
Beutrixx <128897662+Beutrixx@users.noreply.github.com>
2025-03-04 22:28:46 +0800

    final

--- JS BASICS 3/main.js
+++ JS BASICS 3/main.js
@@ -1,11 +1,28 @@
-const numTxt = document.getElementById("elements");
-const numTxt2 = document.getElementById("sum");
-
+let numbers = [];
 
 function insert() {
-
+    const num1 = document.getElementById('num1').value;
+    if (num1 !== '') {
+        const number = parseFloat(num1);
+        if (!isNaN(number)) {
+            numbers.push(number);
+            updateDisplay();
+        } else {
+            alert('Please enter a valid number');
+        }
+    } else {
+        alert('Please enter a number');
+    }
 }
 
-function del() {
+function deleteAll() {
+    numbers = [];
+    updateDisplay();
+}
 
+function updateDisplay() {
+    document.getElementById('elements').innerText = numbers.join(', ');
+    document.getElementById('sum').innerText = numbers.reduce((a, b) => a + b, 0);
+    document.getElementById('highest').innerText = Math.max(...numbers);
+    document.getElementById('lowest').innerText = Math.min(...numbers);
 }
